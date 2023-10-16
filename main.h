ff2846f153f26ec932e53c81c0af7e657f9d9d01
Mona <munaaljaali9375@gmail.com>
2023-10-16 16:19:19 -0700

    update: header file

--- main.h
+++ main.h
@@ -1,4 +1,19 @@
+#include <stdarg.h>
 #ifndef MAIN_H
 #define MAIN_H
+/**
+ * struct specifier - struct specifier
+ * @_c: conversion specifier
+ * @f: the function associated
+ */
+typedef struct specifier
+{
+	char c;
+	int (*f)(va_list ap);
+}spec;
 int _printf(const char *format, ...);
+int print_char(va_list ap);
+int print_string(va_list ap);
+int print_mod(va_list ap);
+
 #endif
